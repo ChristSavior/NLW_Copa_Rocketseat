e27a02e1d3ca7f1a34a713ef2cee84491cbf103f
Rodrigo Guilherme <rodrigog.a@hotmail.com>
2022-11-06 13:36:23 -0300

    Projeto final

---
+++ main.js
@@ -0,0 +1,28 @@
+function createGame(player1, hour, player2){
+    return `
+        <li>
+            <img src="./assets/icon-${player1}.svg" alt="Ícone do ${player1}">
+            <strong>${hour}</strong>
+            <img src="./assets/icon-${player2}.svg" alt="Ícone da ${player2}">
+        </li>`
+}
+
+
+
+
+function createCard(date, day, games) {
+    return `<div class="card">
+    <h2>${date} <span>${day}</span></h2>           
+    <ul>
+       ${games}  
+    </ul>
+</div>
+`
+}
+
+
+
+ document.querySelector("#cards").innerHTML =
+       
+        createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) + createCard('28/11', 'segunda', createGame('portugal', '13:00', 'cameroon'))
+    
